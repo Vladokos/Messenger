d7f0420a06c8a62c7f6bb0e067c3bb6c01249dbb
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-21 23:55:24 +0400

    added certify page

--- Client/src/components/certify/certify.tsx
+++ Client/src/components/certify/certify.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Login from "./login";
 import Register from "./register";
 
+import "../../assets/css/certify/certify.css";
+
 export default function Certify() {
-  return <>{<Login />}</>;
+  //false = used reg side. true = used log side
+  const [login, setLogin] = useState(false);
+
+  const changeSide = () => setLogin(!login);
+
+  return (
+    <div className="form">
+      <div className="container">
+        <div className="form__inner">
+          <div className="backSide">
+            <div className={!login ? "regMessage" : "hidden"}>
+              <p className="title">Don't have an account?</p>
+              <p>Sign up to save all your graph.</p>
+              <button onClick={changeSide}>Sign Up</button>
+            </div>
+            <div className={login ? "logMessage" : "hidden"}>
+              <p className="title">Have an account?</p>
+              <p>Log in to see all your collection.</p>
+              <button onClick={changeSide}>Log In</button>
+            </div>
+          </div>
+          <div className={!login ? "frontSide log" : "frontSide reg"}>
+            {!login ? <Login /> : <Register />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
