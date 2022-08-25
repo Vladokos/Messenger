fa0bd2784925873b7e186adc69ac7761387ed031
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-25 22:23:10 +0400

    added custom input

--- Client/src/components/certify/register.tsx
+++ Client/src/components/certify/register.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 
+import UnderlineInput from "../customInput/underlineInput";
+
 import "../../assets/css/certify/register.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const navigate = useNavigate();
+
+  const signUp = () => {
+    navigate("/main")//local storage 
+  }
+
   return (
     <div className="register">
       <h2>SIGN UP</h2>
       <div className="inputs">
-        <input type="text" placeholder="NAME" />
-        <input type="text" placeholder="EMAIL" />
-        <input type="password" placeholder="PASSWORD" />
+        <UnderlineInput placeholder="NAME" />
+        <UnderlineInput placeholder="EMAIL" />
+        <UnderlineInput type="password" placeholder="PASSWORD" />
       </div>
-      <button>SIGN UP</button>
+      <button onClick={signUp}>SIGN UP</button>
     </div>
   );
 }
