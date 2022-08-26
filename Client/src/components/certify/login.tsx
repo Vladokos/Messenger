6f61c1c187e9fb9776cdd2acac0de19a12f43f19
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-26 23:14:44 +0400

    added underline

--- Client/src/components/certify/login.tsx
+++ Client/src/components/certify/login.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import UnderlineInput from "../customInput/underlineInput";
 
 import "../../assets/css/certify/login.css";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+
+  const logIn = () => {
+    navigate("/main")//local storage 
+  }
+
   return (
     <div className="login">
       <h2>LOG IN</h2>
       <div className="inputs">
-        <input type="text" placeholder="EMAIL" />
-        <input type="password" placeholder="PASSWORD" />
+        <UnderlineInput placeholder="EMAIL" />
+        <UnderlineInput type="password" placeholder="PASSWORD" />
       </div>
       <Link to="/reset/password">FORGET PASSWORD?</Link>
-      <button>LOG IN</button>
+      <button onClick={logIn}>LOG IN</button>
     </div>
   );
 }
