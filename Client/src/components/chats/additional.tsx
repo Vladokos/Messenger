7db2e993f501a8f7840504988a587273a19b9657
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-08-29 23:44:50 +0400

    added additional popup

---
+++ Client/src/components/chats/additional.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../../assets/css/chats/additional.css";
+
+export default function Additional() {
+  return(
+  <div className="additional">
+    <ul>
+      <li>New Chat</li>
+      <li>Create Group</li>
+    </ul>
+  </div>);
+}
